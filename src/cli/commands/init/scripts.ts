1de72013e8f058995ab4293c82911eeccd772e3c
bartek2060 <71974085+bartek2060@users.noreply.github.com>
2024-12-08 23:28:23 +0100

    fix

--- src/cli/commands/init/scripts.ts
+++ src/cli/commands/init/scripts.ts
@@ -1,9 +1,8 @@
 import { BUILD_FOLDER_NAME } from "../../../constants";
 
 const generateScripts = (designDir: string) => ({
-    "design-forge": "ts-node ./packages/design-forge/src/cli/index.ts",
+    "design-forge:build": `npx design-forge build -c ${designDir}/config.ts`,
     "design-forge:watch": `nodemon --watch ${designDir} --ignore '${designDir}/${BUILD_FOLDER_NAME}' -e js,jsx,ts,tsx,css,scss,json --exec "npm run design-forge:build"`,
-    "design-forge:build": `npm run design-forge build ${designDir}/config.ts`,
 });
 
 export default generateScripts;
